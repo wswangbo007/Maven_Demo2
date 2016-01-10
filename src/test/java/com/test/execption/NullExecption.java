99910b5c3146c038e0aa0a7d523c856d6630c181
wswangbo007 <527882238@qq.com>
2016-01-10 16:23:08 +0800

    自定义异常

---
+++ src/test/java/com/test/execption/NullExecption.java
@@ -0,0 +1,21 @@
+package com.test.execption;
+
+/**
+ * 自定义异常
+ * @author Administrator
+ *
+ */
+public class NullExecption extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public NullExecption(){}
+	
+	/**
+	 * 自定义异常
+	 * @param message
+	 */
+	public NullExecption(String message){
+		super(message);
+	}
+}
